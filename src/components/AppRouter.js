545f97291686d614421123e6a6a6f8d661da3b5d
Nick <xa12850328@student.karazin.ua>
2022-04-23 22:29:42 +0300

    Try fix netlify errors

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,9 +1,10 @@
 import React, {useContext} from 'react';
+
+import {useAuthState} from "react-firebase-hooks/auth";
+import {Context} from "../index";
 import {Redirect, Route, Switch} from "react-router-dom";
 import {privateRoutes, publicRoutes} from "../routes";
 import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/consts";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {Context} from "../index";
 
 const AppRouter = () => {
     const {auth} = useContext(Context);
